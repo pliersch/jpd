8f31d6cacca7e244595e282246f4406e9dc0eb84
Patrick Liersch <hourby@gmail.com>
2024-03-18 12:36:17 +0100

    - re-enable sidenav

--- projects/jpd-core/src/lib/components/navigation/sidenav/sidenav.component.ts
+++ projects/jpd-core/src/lib/components/navigation/sidenav/sidenav.component.ts
@@ -5,28 +5,27 @@ import { MatIcon } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { IsActiveMatchOptions, RouterLink, RouterLinkActive } from '@angular/router';
-// import { Route } from '@lib/common/interfaces/route';
-// import { RouteDomService } from '@lib/common/services/route-dom.service';
-// import { PhoneActionComponent } from '@lib/components/actions/phone/phone-action.component';
-// import { ThemeToggleActionComponent } from '@lib/components/actions/theme-toggle/theme-toggle-action.component';
-// import { Observable, of } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { Route, RouteDomService } from '../../../common';
+import { PhoneActionComponent } from '../../actions/phone/phone-action.component';
+import { ThemeToggleActionComponent } from '../../actions/theme-toggle/theme-toggle-action.component';
 
 @Component({
   selector: 'a4w-sidenav',
   standalone: true,
-  imports: [CommonModule, MatListModule, RouterLinkActive, RouterLink, MatSidenavModule, MatIcon, MatIconButton],
+  imports: [CommonModule, MatListModule, RouterLinkActive, RouterLink, MatSidenavModule, MatIcon, MatIconButton, ThemeToggleActionComponent, PhoneActionComponent],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
-export class SidenavComponent /*implements OnInit*/ {
+export class SidenavComponent implements OnInit {
 
-  // route$: Observable<Route>;
-  // linkActiveOptions: IsActiveMatchOptions;
-  //
-  // constructor(private routeDomService: RouteDomService,) { }
-  //
-  // ngOnInit(): void {
-  //   this.route$ = of(this.routeDomService.getRouteDom());
-  //   this.linkActiveOptions = this.routeDomService.getIsActiveMatchOptions()
-  // }
+  route$: Observable<Route>;
+  linkActiveOptions: IsActiveMatchOptions;
+
+  constructor(private routeDomService: RouteDomService,) { }
+
+  ngOnInit(): void {
+    this.route$ = of(this.routeDomService.getRouteDom());
+    this.linkActiveOptions = this.routeDomService.getIsActiveMatchOptions()
+  }
 }
