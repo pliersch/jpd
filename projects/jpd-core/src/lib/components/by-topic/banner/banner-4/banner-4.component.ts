dc5de82bcf08bd4ef93caaf8ad6f3f5991f9a272
Patrick Liersch <hourby@gmail.com>
2024-04-11 14:51:29 +0200

    - banner 4

--- projects/jpd-core/src/lib/components/by-topic/banner/banner-4/banner-4.component.ts
+++ projects/jpd-core/src/lib/components/by-topic/banner/banner-4/banner-4.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { MatAnchor } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'a4w-banner-4',
   standalone: true,
-  imports: [],
+  imports: [
+    MatAnchor,
+    MatIcon
+  ],
   templateUrl: './banner-4.component.html',
   styleUrl: './banner-4.component.scss'
 })
