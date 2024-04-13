8a411073c4c1176f860bcec48ee110c02ab2d75f
Patrick Liersch <hourby@gmail.com>
2024-04-13 14:00:19 +0200

    - card42 improve mobile

--- projects/jpd-core/src/lib/components/by-topic/cards/card4-2/card42.component.ts
+++ projects/jpd-core/src/lib/components/by-topic/cards/card4-2/card42.component.ts
@@ -1,7 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  MatAccordion,
+  MatExpansionModule,
+  MatExpansionPanel,
+  MatExpansionPanelDescription,
+  MatExpansionPanelTitle
+} from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
-import { FragmentDirective } from '../../../../common';
+import { Observable } from 'rxjs';
+import { BreakpointService, Dimension, FragmentDirective } from '../../../../common';
 import { TitleSubtitles } from '../../../../content';
 import { BaseComponent } from '../../../core/base/base.component';
 
@@ -12,14 +20,18 @@ export interface Card42Model {
 @Component({
   selector: 'a4w-card42',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, MatAccordion, MatExpansionModule, MatExpansionPanel, MatExpansionPanelTitle, MatExpansionPanelDescription],
   templateUrl: './card42.component.html',
   styleUrls: ['./card42.component.scss']
 })
 export class Card42Component extends BaseComponent<Card42Model> {
 
-  constructor(override fragment?: FragmentDirective) {
-    super('Card42', fragment)
+  dim$: Observable<string>;
+
+  constructor(breakpointService: BreakpointService, override fragment?: FragmentDirective) {
+    super('Card42', fragment);
+    this.dim$ = breakpointService.dimension$;
   }
 
+  protected readonly Dimension = Dimension;
 }
